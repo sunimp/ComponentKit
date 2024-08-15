319877f387b0f5d99340fffa2bbf2e9b5b0f631a
杨光 <yangguang@webull.com>
2024-08-15 12:14:25 +0800

    fix: bump version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.10
 
 import PackageDescription
 
@@ -13,12 +13,12 @@ let package = Package(
                     targets: ["ComponentKit"]),
         ],
         dependencies: [
-            .package(url: "https://github.com/Juanpe/SkeletonView.git", from: "1.7.0"),
+            .package(url: "https://github.com/Juanpe/SkeletonView.git", from: "1.31.0"),
             .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1")),
-            .package(url: "https://github.com/horizontalsystems/HUD.Swift.git", .upToNextMajor(from: "2.0.0")),
-            .package(url: "https://github.com/horizontalsystems/SectionsTableView.Swift.git", .upToNextMajor(from: "1.0.0")),
-            .package(url: "https://github.com/horizontalsystems/ThemeKit.Swift.git", .upToNextMajor(from: "2.0.0")),
-            .package(url: "https://github.com/horizontalsystems/UIExtensions.Swift.git", .upToNextMajor(from: "1.0.0")),
+            .package(url: "https://github.com/sunimp/HUD.Swift.git", .upToNextMajor(from: "2.0.1")),
+            .package(url: "https://github.com/sunimp/SectionsTableView.Swift.git", .upToNextMajor(from: "1.0.1")),
+            .package(url: "https://github.com/sunimp/ThemeKit.Swift.git", .upToNextMajor(from: "2.0.3")),
+            .package(url: "https://github.com/sunimp/UIExtensions.Swift.git", .upToNextMajor(from: "1.0.2")),
         ],
         targets: [
             .target(
