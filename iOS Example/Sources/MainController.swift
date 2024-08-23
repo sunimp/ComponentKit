52051b4bf3be29f3a396713bfaae2d1e63dbc9de
杨光 <yangguang@webull.com>
2024-08-23 17:06:14 +0800

    fix: navigation bar shadow

--- iOS Example/Sources/MainController.swift
+++ iOS Example/Sources/MainController.swift
@@ -19,41 +19,35 @@ class MainController: ThemeTabBarController {
         let buttonsTitle = "Buttons"
         let buttonsController = ButtonsController()
         buttonsController.title = buttonsTitle
-
-        let cellsTitle = "Cells"
-        let cellsController = CellsController()
-        cellsController.title = cellsTitle
-        
-        let experimentalTitle = "Experimental"
-        let experimentalView = ExperimentalView()
-        let experimentalController = UIHostingController(rootView: experimentalView)
-        experimentalController.title = experimentalTitle
-        
-        let buttonsNav = ThemeNavigationController(rootViewController: buttonsController)
-        buttonsNav.tabBarItem = ThemeTabBarItem(
+        buttonsController.tabBarItem = ThemeTabBarItem(
             title: buttonsTitle,
             image: UIImage(systemName: "button.programmable.square.fill")?.withRenderingMode(.alwaysTemplate).resize(CGSize(width: 28, height: 25)),
             tag: 0
         )
         
-        let cellsNav = ThemeNavigationController(rootViewController: cellsController)
-        cellsNav.tabBarItem = ThemeTabBarItem(
+        let cellsTitle = "Cells"
+        let cellsController = CellsController()
+        cellsController.title = cellsTitle
+        cellsController.tabBarItem = ThemeTabBarItem(
             title: cellsTitle,
             image: UIImage(systemName: "tablecells.badge.ellipsis")?.withRenderingMode(.alwaysTemplate).resize(CGSize(width: 28, height: 22)),
             tag: 0
         )
         
-        let experimentalNav = ThemeNavigationController(rootViewController: experimentalController)
-        experimentalNav.tabBarItem = ThemeTabBarItem(
+        let experimentalTitle = "Experimental"
+        let experimentalView = ExperimentalView()
+        let experimentalController = UIHostingController(rootView: experimentalView)
+        experimentalController.title = experimentalTitle
+        experimentalController.tabBarItem = ThemeTabBarItem(
             title: experimentalTitle,
             image: UIImage(named: "TabBar Icon")?.resize(CGSize(width: 24, height: 24)),
             tag: 0
         )
         
         viewControllers = [
-            buttonsNav,
-            cellsNav,
-            experimentalNav
+            ThemeNavigationController(rootViewController: buttonsController),
+            ThemeNavigationController(rootViewController: cellsController),
+            ThemeNavigationController(rootViewController: experimentalController)
         ]
 
         self.selectedIndex = selectedIndex
